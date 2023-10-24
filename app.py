b281abc0ae9196f1900c6d485ac25c209ff32301
SamSantos1jz <samsantos1jz@gmail.com>
2023-10-24 17:00:02 -0300

    feat: second commit

--- app.py
+++ app.py
@@ -18,18 +18,28 @@ class App(ctk.CTk):
         self.all_system()
 
     def layout_config(self):
-        self.title("Sistema de cadastro de oficina")
+        self.title("Sistema de cadastro de Oficina")
         self.geometry("700x500")
 
     def appaerence(self):
         self.lb_apm = ctk.CTkLabel(self, text="Tema",  bg_color="transparent", 
                                    text_color=['#000','#fff']).place(x=50, y=430)
         self.opt_apm = ctk.CTkOptionMenu(self, values=["Light", "Dark", "System"],
-                                          command=self.change_apm).place(x=50, y=460)
-    
+                                        command=self.change_apm).place(x=50, y=460)
+        
+
     def all_system(self):
-        Frame = ctk.CTkFrame(self, width=700, height=70, corner_radius=0, bg_color="teal", 
+        frame = ctk.CTkFrame(self, width=700, height=70, corner_radius=0, bg_color="teal", 
                              fg_color="teal").place(x=0, y=0)
+        title_main= ctk.CTkLabel(frame, text="Sistema Car Henrique", font=("Century Gothic bold", 24),
+                                  text_color="#fff", bg_color="teal").place(x=230, y=20)
+        
+        span = ctk.CTkLabel(self, text="Por favor preencha todos os campos.", font=("Century Gothic bold",
+                                         12), text_color=["#000", "#fff"]).place(x=50, y=70)
+
+        # labels
+
+
 
 
 
