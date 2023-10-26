7a8f2d521385ddafcd7825daea188a6604a145c1
SamSantos1jz <samsantos1jz@gmail.com>
2023-10-25 21:28:04 -0300

    feat: commit-campos

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pathlib
 from openpyxl import workbook
 
 
+
 # colocando aparencia padrao do sistema
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
@@ -16,10 +17,11 @@ class App(ctk.CTk):
         self.layout_config()
         self.appaerence()
         self.all_system()
+        
 
     def layout_config(self):
         self.title("Sistema de cadastro de Oficina")
-        self.geometry("700x500")
+        self.geometry("930x500")
 
     def appaerence(self):
         self.lb_apm = ctk.CTkLabel(self, text="Tema",  bg_color="transparent", 
@@ -29,24 +31,43 @@ class App(ctk.CTk):
         
 
     def all_system(self):
-        frame = ctk.CTkFrame(self, width=700, height=70, corner_radius=0, bg_color="teal", 
+        
+        frame = ctk.CTkFrame(self, width=1000, height=70, corner_radius=0, bg_color="teal", 
                              fg_color="teal").place(x=0, y=0)
-        title_main= ctk.CTkLabel(frame, text="Sistema Car Henrique", font=("Century Gothic bold", 24),
-                                  text_color="#fff", bg_color="teal").place(x=230, y=20)
+        title_main= ctk.CTkLabel(frame, text="SISTEMA CAR HENRIQUE", font=("arial", 24),
+                                  text_color="#fff", bg_color="teal").place(x=360, y=20)
         
         span = ctk.CTkLabel(self, text="Por favor preencha todos os campos.", font=("Century Gothic bold",
                                          10), text_color=["#000", "#fff"]).place(x=50, y=70)
 
         # Entrys
-        name_entry = ctk.CTkEntry(self, width=350, font=("Century Ghotic", 16), fg_color="transparent")
-        contact_entry = ctk.CTkEntry(self, width=350, font=("Century Ghotic", 16), fg_color="transparent")
-        adress_entry = ctk.CTkEntry(self, width=350, font=("Century Ghotic", 16), fg_color="transparent")
-        cel_entry = ctk.CTkEntry(self, width=350, font=("Century Ghotic", 16), fg_color="transparent")
+        name_entry = ctk.CTkEntry(self, width=350, font=("Century Ghotic bold", 16), fg_color="transparent")
+        tel_entry = ctk.CTkEntry(self, width=200, font=("Century Ghotic bold", 16), fg_color="transparent")
+        adress_entry = ctk.CTkEntry(self, width=350, font=("Century Ghotic bold", 16), fg_color="transparent")
+        cel_entry = ctk.CTkEntry(self, width=200, font=("Century Ghotic bold", 16), fg_color="transparent")
+        adress_num_entry = ctk.CTkEntry(self, width=100, font=("Century Ghotic bold", 16), fg_color="transparent")
+        date_entry = ctk.CTkEntry(self, width=100, font=("Century Gothic bold", 16), fg_color="transparent")
+        payment_entry = ctk.CTkEntry(self, width=150, font=("Century Gothic bold", 16), fg_color="transparent")
+        car_entry = ctk.CTkEntry(self, width=250, font=("Century Gothic bold", 16), fg_color="transparent")
+
+
+        # ComboBox 
+        payment_combo_box = ctk.CTkComboBox(self, values=["---","PAGO", "PENDENTE"], font=("Century Ghotic bold", 14))
+        payment_combo_box.set("---")
+
+        # OBSERVAÇÕES
+        obs_entry = ctk.CTkTextbox(self, width=830, height=150, font=("arial", 18), border_color="#aaa", border_width=2, fg_color="transparent")
 
 
         # labels
-        lb_name = ctk.CTkLabel(self, text="CLIENTE", font=("Century Gothic bold",
+        lb_date = ctk.CTkLabel(self, text="Data", font=("Century Gothic bold",
+                                    13), text_color=["#000", "#fff"])
+
+        lb_name = ctk.CTkLabel(self, text="Cliente", font=("Century Gothic bold",
                                          13), text_color=["#000", "#fff"])
+        
+        lb_name_car = ctk.CTkLabel(self, text="Carro", font=("Century Gothic bold", 
+                                        13), text_color=["#000", "#fff"])
         
         lb_celular = ctk.CTkLabel(self, text="Celular", font=("Century Gothic bold",
                                          13), text_color=["#000", "#fff"])
@@ -57,17 +78,36 @@ class App(ctk.CTk):
         lb_endereco = ctk.CTkLabel(self, text="Endereço", font=("Century Gothic bold",
                                          13), text_color=["#000", "#fff"])
         
-        lb_cep = ctk.CTkLabel(self, text="CEP", font=("Century Gothic bold",
-                                         13), text_color=["#000", "#fff"])
-        
-        lb_car_name = ctk.CTkLabel(self, text="Automovel", font=("Century Gothic bold",
+        lb_payment = ctk.CTkLabel(self, text="Visto de pagamento", font=("Century Gothic bold",
                                          13), text_color=["#000", "#fff"])
        
         lb_obs = ctk.CTkLabel(self, text="Observação", font=("Century Gothic bold",
                                          13), text_color=["#000", "#fff"])
 
+        # POSIÇÃO DOS ELEMENTOS DA TELA
+        lb_date.place(x=50, y=120)
+        date_entry.place(x=50, y=150)
+        
+        lb_name.place(x=160, y=120)
+        name_entry.place(x=160, y=150)
 
+        lb_endereco.place(x=520, y=120)
+        adress_entry.place(x=520, y=150)
 
+        lb_telefone.place(x=50, y=200)
+        tel_entry.place(x=50, y=230)
+
+        lb_celular.place(x=260, y=200)
+        cel_entry.place(x=260, y=230)
+
+        lb_name_car.place(x=470, y=200)
+        car_entry.place(x=470, y=230)
+
+        lb_payment.place(x=730, y=200)
+        payment_combo_box.place(x=730, y=230)
+
+        lb_obs.place(x=50, y=270)
+        obs_entry.place(x=50, y=300)
 
 
     def change_apm(self, new_appaerence_mode):
